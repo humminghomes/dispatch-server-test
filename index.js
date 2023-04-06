8ddf2e5068bd1a5a497fec12370583d95185283d
Will Brandin <will.brandin@humminghomes.com>
2023-04-06 08:17:53 -0600

    put outbound request in own file. increase time between polling events.

--- index.js
+++ index.js
@@ -1,60 +1,20 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
-
 const PORT = process.env.PORT || 8080;
+
+// External Job Route
+const jobsRouter = require("./router/jobs");
+const startDispatchPolling = require("./polling/dispatch-outbound");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-
-const jobsRouter = require("./router/jobs");
-
 app.use("/api/dispatch", jobsRouter);
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
-  startPolling();
+  startDispatchPolling();
 });
-
-// extrea
-const Pollinator = require("pollinator");
-
-var key = process.env["DISPATCH_CLIENT_ID"];
-var secret = process.env["DISPATCH_CLIENT_SECRET"];
-
-var url = "https://connect-sbx.dispatch.me/";
-var max_messages = 10;
-var out_payload = `{"maxNumberOfMessages":${max_messages}}`;
-var secret = Buffer.from(secret, "hex");
-const crypto = require("crypto");
-var out_signature = crypto
-  .createHmac("sha256", secret)
-  .update(out_payload, "utf8")
-  .digest("hex");
-
-var out_headers = {
-  "Content-Type": "application/json",
-  "X-Dispatch-Key": key,
-  "X-Dispatch-Signature": out_signature,
-};
-
-var out_request = {
-  method: "POST",
-  headers: out_headers,
-  body: out_payload,
-};
-
-const startPolling = async () => {
-  try {
-    let res = await fetch(url + "agent/out", out_request);
-    const msg = JSON.stringify(res.body);
-    console.log(msg);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const poller = new Pollinator(startPolling);
-poller.start();
---
+++ polling/dispatch-outbound.js
@@ -0,0 +1,46 @@
+const Pollinator = require("pollinator");
+
+var key = process.env["DISPATCH_CLIENT_ID"];
+var secret = process.env["DISPATCH_CLIENT_SECRET"];
+
+var url = "https://connect-sbx.dispatch.me/agent/out";
+var max_messages = 10;
+var out_payload = `{"maxNumberOfMessages":${max_messages}}`;
+var secret = Buffer.from(secret, "hex");
+const crypto = require("crypto");
+var out_signature = crypto
+  .createHmac("sha256", secret)
+  .update(out_payload, "utf8")
+  .digest("hex");
+
+var out_headers = {
+  "Content-Type": "application/json",
+  "X-Dispatch-Key": key,
+  "X-Dispatch-Signature": out_signature,
+};
+
+var out_request = {
+  method: "POST",
+  headers: out_headers,
+  body: out_payload,
+};
+
+const dispatchRequest = async () => {
+  try {
+    let res = await fetch(url, out_request);
+    const msg = JSON.stringify(res.body);
+    console.log(res.status, msg);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const startDispatchPolling = () => {
+  // Polls every 5 seconds
+  const poller = new Pollinator(dispatchRequest, {
+    delay: 30000,
+  });
+  poller.start();
+};
+
+module.exports = startDispatchPolling;
